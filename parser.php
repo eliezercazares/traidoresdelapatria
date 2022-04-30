a8b88479b5c325aa06978202987fb7c225c7b63b
Eliezer Cazares <eliezer.cazares@gmail.com>
2022-04-29 20:10:02 -0500

    agregando valores de estado y partido

--- parser.php
+++ parser.php
@@ -45,6 +45,52 @@ if ($handle) {
         $distrito = $columns[12];
         $dob = $columns[13];
 
+        // partido
+        $array_partidos = array(
+            0 => 'Sin partido', 
+            1 => 'PAN', 
+            2 => 'PRI', 
+            3 => 'PRD', 
+            4 => 'MC', 
+            5 => 'PVEM', 
+            6 => 'Morena' 
+        );
+        // estado
+        $array_estados = array( 
+            1 => 'Aguascalientes'
+            2 => 'Baja California'
+            3 => 'Baja California Sur'
+            4 => 'Campeche'
+            5 => 'Chiapas'
+            6 => 'Chihuahua'
+            7 => 'Ciudad de México'
+            8 => 'Coahuila'
+            9 => 'Colima'
+            10 => 'Durango'
+            11 => 'Estado de México'
+            12 => 'Guanajuato'
+            13 => 'Guerrero'
+            14 => 'Hidalgo'
+            15 => 'Jalisco'
+            16 => 'Michoacán'
+            17 => 'Morelos'
+            18 => 'Nayarit'
+            19 => 'Nuevo León'
+            20 => 'Oaxaca'
+            21 => 'Puebla'
+            22 => 'Querétaro'
+            23 => 'Quintana Roo'
+            24 => 'San Luis Potosí'
+            25 => 'Sinaloa'
+            26 => 'Sonora'
+            27 => 'Tabasco'
+            28 => 'Tamaulipas'
+            29 => 'Tlaxcala'
+            30 => 'Veracruz'
+            31 => 'Yucatán'
+            32 => 'Zacatecas'
+        );
+
         // si hay una imagen en el csv y si elegimos guardar las imagenes entones las descargamos
         if (!empty($imagen) && $storeImages == true) {
             file_put_contents($destination . basename($imagen), file_get_contents($imagen));
@@ -52,6 +98,10 @@ if ($handle) {
 
         // remover el dominio para hacer referencia al archivo descargado local
         $imagen = str_replace("http://sitl.diputados.gob.mx/LXV_leg/fotos_lxvconfondo/", "", $columns[6]);
+
+        // cambiamos los ids por los nombres de partido y estado
+        $partido = $array_partidos[$partido];
+        $estado = $array_estados[$estado];
 
         // crear el registro en la tabla con una referencia al filename de las fotos descargadas
         $query = "INSERT INTO traidores VALUES ('" . $nombre . "', '" . $partido . "', '" . $imagen . "', '" . $link . "', '" . $periodo . "', '" . $estado . "', '" . $distrito . "', '" . $dob . "')";
